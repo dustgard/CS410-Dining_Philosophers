069c32b102238dd31fd1f5c4a87b1adc75f17f0b
Dustin Gardner <dustin.j.gardner@gmail.com>
2024-03-08 13:03:31 -0700

    Building Classes for Project

--- src/DiningTable.java
+++ src/DiningTable.java
@@ -1,5 +1,10 @@
 
 public class DiningTable {
+
+    private int NUM_PHILOSOPHER = 5;
+    private int NUM_CHOPSTICKS = NUM_PHILOSOPHER*2;
+
     public static void main(String[] args) {
+
     }
 }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,2 +1,33 @@
-public class Philosopher {
+public class Philosopher implements Runnable {
+
+    private String name;
+    private Thread thread;
+    private ChopStick chopStickRight, chopStickLeft;
+
+    public Philosopher(){
+
+    }
+
+    public Philosopher(String name) {
+        this.name = name;
+        thread = new Thread(this, name);
+    }
+
+    public void assignChopSticks(ChopStick chopStickRight, ChopStick chopStickLeft){
+        this.chopStickLeft = chopStickLeft;
+        this.chopStickRight = chopStickRight;
+    }
+
+    public void eatRice(){
+
+    }
+
+    public void takeChopStick(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
